b8ea0b8e7d9db114b24a58893ca154999496c447
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-08-11 13:53:47 -0500

    SpriteKit spawns story root correctly on game scene.

--- PruebasiPad/ViewControllers/SpriteKit/NodeMapScene.swift
+++ PruebasiPad/ViewControllers/SpriteKit/NodeMapScene.swift
@@ -11,11 +11,22 @@ import SpriteKit
 
 class NodeMapScene: SKScene {
     
+    let mainNodeColor = "light green node"
+    let childNodeColor = "yellow node"
+    
     private var label : SKLabelNode?
     private var node : SKSpriteNode?
     var storyId: String? //The story uuid we selected on the collection view is injected here.
     
-    var storyLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin ")
+    var storyLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    let realm = try! Realm()
+    
+    // Tree Navigation Variables:
+    var visitedNodes: [StoryNode] = []
+    var actualNode: StoryNode?
+    
+    
+    // MARK: - Override Functions
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "story background")
@@ -25,20 +36,41 @@ class NodeMapScene: SKScene {
         addChild(background)
         
         // Get access to Story so we can build the UI
-        do {
-            let realm = try Realm()
-            let story = realm.objects(Story.self).filter(NSPredicate(format: "uuid CONTAINS %@", self.storyId!)).first //Query to get story based on the id
-            storyLabel.fontSize = 50
-            storyLabel.position = CGPoint(x: 20, y: 20)
-            storyLabel.text = story?.storyName ?? " "
-            storyLabel.zPosition = 1
-            storyLabel.horizontalAlignmentMode = .left
-            addChild(storyLabel)
-        } catch {
-            print("Unable to open realm database.")
-        }
-        
-        
+        let story = realm.objects(Story.self).filter(NSPredicate(format: "uuid CONTAINS %@", self.storyId!)).first //Query to get story based on the id
+        storyLabel.fontSize = 50
+        storyLabel.position = CGPoint(x: 20, y: 20)
+        storyLabel.text = story?.storyName ?? " "
+        storyLabel.zPosition = 1
+        storyLabel.horizontalAlignmentMode = .left
+        addChild(storyLabel)
+
+        //Ya tenemos el story, ahora obtenemos su info
+        spawnAsRootNode(node: story!.root!)
         
         }
+    
+    // MARK: - Utility Functions
+    
+    func spawnAsRootNode(node: StoryNode){
+        visitedNodes.append(node) //We add the node to the visited list
+        actualNode = node //We change the focus of the actual node
+        let rootNode = SKSpriteNode(imageNamed: mainNodeColor)
+        rootNode.position = CGPoint(x: size.width/2, y: size.height/2)
+        rootNode.size = CGSize(width: 200, height: 200)
+        
+        let nodeName = SKLabelNode(text: node.chapter?.chapterTitle ?? " ")
+        nodeName.fontName = "HelveticaNeue-Thin"
+        nodeName.fontSize = 25
+        nodeName.fontColor = .black
+        nodeName.zPosition = 100
+        nodeName.horizontalAlignmentMode = .center
+        rootNode.addChild(nodeName)
+        
+        addChild(rootNode)
+        
+        //Spawn everychildren around this node.
+        
+    }
+    
+    
 }
